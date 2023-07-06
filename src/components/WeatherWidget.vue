c76819fc79d1f5b8919b4dc31c2705d84ee52443
SegoroCampus <bonosegoro@student.uir.ac.id>
2023-07-06 14:19:09 +0700

    first commit

---
+++ src/components/WeatherWidget.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="weather-widget">
+    <h2 class="widget-title">Weather Widget</h2>
+    <div class="location-input">
+      <label for="location">Enter Location:</label>
+      <input type="text" id="location" v-model="location" />
+      <button @click="fetchWeatherData">Get Weather</button>
+    </div>
+    <div v-if="weatherData" class="weather-data">
+      <p class="location">Location: {{ weatherData.name }}</p>
+      <p v-if="weatherData.main" class="temperature">
+        Temperature: {{ weatherData.main.temp }}°C
+      </p>
+      <p v-if="weatherData.weather" class="description">
+        Description: {{ weatherData.weather[0].description }}
+      </p>
+    </div>
+    <p v-else>Loading weather data...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      location: '',
+      weatherData: null
+    };
+  },
+  methods: {
+    async fetchWeatherData() {
+      try {
+        const apiKey = 'b7bfca7b27a3485144fea086c50d09dc';
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${apiKey}`;
+
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        this.weatherData = data;
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.weather-widget {
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+  background-color: #f2f2f2;
+  border-radius: 0 0 15px 15px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.weather-widget input {
+  width: 200px;
+  padding: 10px;
+  margin-right: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+}
+
+.weather-widget label {
+  display: block;
+  margin-bottom: 5px;
+  color: #333;
+  font-size: 16px;
+}
+
+.weather-widget button {
+  margin-top: 10px;
+  padding: 10px 20px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.widget-title {
+  margin-top: 0;
+  color: #333;
+}
+
+.location-input {
+  margin-bottom: 10px;
+}
+
+.weather-data {
+  margin-top: 10px;
+}
+
+.location {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.temperature {
+  font-size: 24px;
+  color: #ff5722;
+}
+
+.description {
+  font-size: 16px;
+}
+</style>
